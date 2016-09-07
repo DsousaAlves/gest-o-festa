6fa2e5693c1d98651fe2a0874d817c6c26bfdc09
David de Sousa <davidsousaalves@gmail.com>
2016-09-06 21:25:11 -0300

    Listar e add convidados.

--- gestao-festa/src/main/java/com/dsousa/gestaoFesta/controller/ConvidadosController.java
+++ gestao-festa/src/main/java/com/dsousa/gestaoFesta/controller/ConvidadosController.java
@@ -3,8 +3,10 @@ package com.dsousa.gestaoFesta.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.dsousa.gestaoFesta.model.Convidado;
 import com.dsousa.gestaoFesta.model.Convidados;
 
 @Controller
@@ -17,7 +19,18 @@ public class ConvidadosController {
 	@RequestMapping("/convidados")
 	public ModelAndView listar(){
 		ModelAndView mv = new ModelAndView("listaConvidados");
+		mv.addObject(new Convidado());
 		mv.addObject("convidados", convidados.todos());
 		return mv;
 	}
+	
+	@RequestMapping(value = "/convidados", method = RequestMethod.POST)
+	public String salvar(Convidado convidado){
+		this.convidados.adicionar(convidado);
+		
+		return "redirect:/convidados";
+	
+	}
+	
+	
 }
